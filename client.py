f4495a98983d330eeeaab3a576b5ce6e1e69e228
Bryan Lee <mylee1995@gmail.com>
2020-04-22 13:54:16 +0900

    Add files via upload

---
+++ client.py
@@ -0,0 +1,176 @@
+
+# Main libraries that I used for creating a chat room are as follows:
+# Socket to establish connection between the server and client
+# Thread to run multiple functions simulatenously
+# json to exchange message in json (dictionary) format
+import socket
+from _thread import *
+import threading
+import os
+import time
+import json
+
+HOST = 'localhost'
+PORT = 33000
+BUFFER_SIZE = 1024
+ADDR = (HOST, PORT)
+# Boolean that indicates the connection with the server. Initially set to False
+CONNECTED = False
+
+
+# User command template for users
+userCMD = "[*] Type <announce> to announce a message to all users in the chat room\n\
+[*] Type <list> to Broadcast a message to all users in the chat room\n\
+[*] Type <private> to send private message to a specific user in the chat room\n"
+
+
+# Creates Socket
+userSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+# This function is called whenever a new message is received from the server.
+def clear():
+    # Checks if the operating system is Windows. If not it uses the other variant.
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
+
+
+# Attempt to connect to the server with given count parameter
+def connect_to_server(count):
+    # If it successfuly connects to the server, set the connected boolean to true.
+    try:
+        userSocket.connect(ADDR)
+        CONNECTED = True
+
+    # Server came online in that time, it can connect.
+    except:
+        print('[*] Could not contact the Chatroom Server at this time.\n[*] The server may be offline or down for maintenance\n[*] Sorry for the inconvenience.')
+        os.sys.exit()
+        # This will exit the program after 5 attempts to connect have been made.
+
+    # If connection has been established, start the receive thread to listen to the server.
+    if CONNECTED:
+        print("[*] Connected to Chatroom Server.")
+        input('[*] Press enter to continue...')
+        RECEIVE_THREAD = threading.Thread(target=receive)
+        RECEIVE_THREAD.start()
+
+
+# Handles receiving message from the server (both announcements and private messages)
+def receive():
+    # if CONNECTED:
+
+    # Keeps track of the all the messages for back up
+    msgHistory = ''
+
+    # This creates a new thread for sending messages and also starts it.
+    SEND_THREAD = threading.Thread(target=send_msg)
+    SEND_THREAD.start()
+
+    # Constantly looks up the message from the server
+    while True:
+
+        # This tries to listen for a message from the server
+        try:
+            # This gets the message and then adds it to the message buffer.
+            message = userSocket.recv(BUFFER_SIZE).decode()
+            msgHistory += message
+
+            # This checks to see if the messaged typed is not the <quit> request
+            # Command. If it isnt then it clears the screen and prints the entirety
+            # Of the message buffer.
+            if message != '<quit>':
+                clear()
+                # print(message)
+                print(msgHistory)
+
+            # This states that if the client does want to send a <quit> request,
+            # It will close the socket and exit this thread.
+            else:
+                userSocket.close()
+                break
+
+        # Error from the client indicating the connection has been lost
+        except OSError:
+            break
+
+
+def formatJSON(msgType='', content=''):
+    # Format JSON Object
+    return {"msgType": msgType, "content": content}
+
+
+def encodeJSON(data):
+    # Format JSON object to string
+    encoder = json.JSONEncoder()
+    return encoder.encode(data)
+
+
+def decodeToJSON(jsonObj):
+    # @ returns dictionary object
+    # Decode JSON formatted string to dictionary object
+    decoder = json.JSONDecoder()
+    return decoder.decode(jsonObj)
+
+
+# send_msg handles sending messages to the server the task of sending messages to the chat room.
+def send_msg():
+    first = False
+    # To determine if user just logged in for the first time in the session
+    if first == False:
+        userName = input("")
+        # Send user name to the server to register the user in the chatroom
+        jdata = formatJSON('<first>', userName)
+        userSocket.send(encodeJSON(jdata).encode())
+        first = True
+    while True:
+        try:
+            # Choose the options
+            cmd = input(
+                "[*] Choose msg options.\n[*] Type <help> to see the list of options\n")
+            # <list> asks the server the list of active users to the server
+            if cmd == '<list>':
+                jdata = formatJSON(cmd)
+                userSocket.send(encodeJSON(jdata).encode())
+            # <private> allows to send private message to specific active user in the chatroom
+            elif cmd == '<private>':
+                target = input("[*] Who do you want to send message to?\n")
+                content = input("[*] Type message you want to send\n")
+                jdata = formatJSON(cmd, content)
+                jdata["target"] = target
+                userSocket.send(encodeJSON(jdata).encode())
+                # print('Private message to {} sent to server!'.format(target))
+            # Provide user command options
+            elif cmd == '<help>':
+                print(userCMD)
+            # Asks the content of the announcement if user selects announce option
+            elif cmd == '<announce>':
+                content = input(
+                    "[*] Type message you want to announce to everyone\n")
+                jdata = formatJSON(cmd, content)
+                userSocket.send(encodeJSON(jdata).encode())
+            # If user wants to quit, let the server know and terminate the current thread.
+            elif cmd == '<quit>':
+                jdata = formatJSON(cmd)
+                userSocket.send(encodeJSON(jdata).encode())
+                break
+            # Let the user know that the cmd input did not meet any of the options
+            else:
+                print(
+                    "[*] Not a valid option. \n")
+
+        # If the server has been shutdown while the client is still online.
+        except:
+            print("Connection to server lost")
+            break
+
+
+def Main():
+    clear()
+    connect_to_server(1)
+
+
+if __name__ == '__main__':
+    Main()
---
+++ server.py
@@ -0,0 +1,231 @@
+# Main libraries that I used for creating a chat room are as follows:
+# Socket to establish connection between the server and client
+# Thread to run multiple functions simulatenously
+# json to exchange message in json (dictionary) format
+import socket
+from _thread import *
+import threading
+import os
+import json
+
+
+# Global variables, the port number between the server and client must be same to establish a connection
+HOST = ''
+PORT = 33000
+BUFFER = 1024
+ADDR = (HOST, PORT)
+
+# Clients and Addresses are dictionary objects that keep track of active users and its ip addresses
+USERS = {}
+ADDRESSES = {}
+
+# General Header that helps
+HEADER = "Server listening on {} \n[*] Type 'help' to see a list of available server commands.".format(
+    PORT)
+
+# This creates a server socket on startup and binds it to the (HOST,PORT)
+SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER.bind(ADDR)
+
+
+def clear():
+    # Checks to see if operating system is Windows, if not it will use the other clearscreen variant.
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
+
+
+def encodeJSON(data):
+    # @ returns String
+    # Format JSON object to string
+    encoder = json.JSONEncoder()
+    return encoder.encode(data)
+
+
+def decodeToJSON(jsonObj):
+    # @ returns JSON object
+    # Decode JSON formatted string to dictionary object
+    decoder = json.JSONDecoder()
+    return decoder.decode(jsonObj)
+
+
+def establish_connections():
+    # Generate a server command thread for general server menu
+    CMD_THREAD = threading.Thread(target=server_cmds)
+    CMD_THREAD.start()
+
+    # Use while true loop to constantly seeks for the message
+    while True:
+
+        # Establish a connection between the serer and the client
+        client_socket, client_address = SERVER.accept()
+        print("[*] {}:{} has connected.".format(client_address[0], client_address[1]))
+
+        # Ask the client for the user name
+        client_socket.send(
+            "[*] Welcome to the chat room!\n[*] Please enter your user name to get started.\n".encode())
+
+        # This section adds the relevant client network information to the ADDRESSES dictionary (IP_Address, Port)
+        ADDRESSES[client_socket] = client_address
+
+        # Generate a new thread to handle the communication between the client and the server
+        CLIENT_THREAD = threading.Thread(
+            target=handle_client, args=(client_socket,))
+        CLIENT_THREAD.start()
+
+
+# Function that handles communication between a single user and the server.
+def handle_client(client):
+
+    # After the above steps are finished it will forever loop until the user
+    # Sends a request to quit.
+    while True:
+        # This tries to constantly listen for data being sent over the connected client Socket
+        try:
+            fromClientString = client.recv(BUFFER).decode()
+            # Decodes JSON formatted string to dictionary object
+            fromClientJSON = decodeToJSON(fromClientString)
+            messageType = fromClientJSON["msgType"]
+            content = fromClientJSON["content"]
+            if messageType == '<first>':
+                # Welcomes and announces the new user
+                client.send(
+                    "\nServer: Welcome {}!\n\nChoose msg options.\nType <help> to see the list of options\n".format(content).encode())
+                message = "\nServer: {} has joined the chat room!".format(
+                    content)
+                announce(message)
+                # Adds the new user name to the USERS dictionary
+                USERS[client] = content
+            elif messageType == "<list>":
+                UserList = ''
+                for u in ADDRESSES:
+                    UserList += '   Client: \'{}\' ~ {} : {}\n'.format(
+                        USERS[u], ADDRESSES[u][0], ADDRESSES[u][1])
+                client.send(
+                    "\n[*] List of users \n {}\n ".format(UserList).encode())
+            elif messageType == "<private>":
+                # Let the user know whether the private message was successfully sent
+                if privateMessage(client, fromClientJSON) == False:
+                    client.send(
+                        "[*] Oops there was an error sending DM, please check the intended username again".encode())
+            elif messageType == "<announce>":
+                announce(content, "\n"+USERS[client]+": ")
+            elif messageType == "<quit>":
+                close_connection(client)
+                break
+            else:
+                announce(content, "\n"+USERS[client]+': ')
+
+        except:
+            continue
+
+
+# Sends private message to a specifc user
+def privateMessage(user, JSONobj):
+    From = USERS[user]
+    Content = JSONobj["content"]
+    To = JSONobj["target"]
+    for x in USERS:
+        if USERS[x] == To:
+            x.send("From {} to {}: {}".format(
+                From, To, Content).encode())
+            return True
+    return False
+
+
+def announce(message, announcer=''):
+    # Announce message to all users
+    try:
+        for u in USERS:
+            u.send("{}{}".format(announcer, message).encode())
+    except:
+        pass
+
+
+def close_connection(client):
+    # Send client "<quit>" to close the connection from client side as well
+    client.send("<quit>".encode())
+
+    # Announce that the user has left the chat room
+    announce("\n[*] {} has left the chat room.".format(USERS[client]))
+
+    # Print that user is is disconnected in server terminal
+    print('[*] User: {} ~ {} : {} is disconnected.'.format(USERS[client],
+                                                           ADDRESSES[client][0], ADDRESSES[client][1]))
+
+    client.close()
+    # Delete client in glboal dictionary objects
+    del ADDRESSES[client]
+    del USERS[client]
+
+
+def server_cmds():
+    while True:
+        # Gets server cmds
+        cmd = input('>>')
+
+        # Help shows the list of command options.
+        if cmd.lower() == 'help':
+            print('[*] announce - announce a message to the chat room.')
+            print('[*] clear - Clear the screen.')
+            print('[*] help - Provides Help information for Server Commands.')
+            # print('[*] KICK - Kick a client from the server.')
+            print('[*] list - Lists the current connections.')
+            print('[*] quit - quits the chat room server')
+
+        # Announce all users from the server
+        elif cmd.lower() == 'announce':
+            message = input('What would you like to announce?\n >> ')
+            announce(message, "\nAnnounce ment from server: ")
+
+        # Clear the server terminal
+        elif cmd.lower() == 'clear':
+            clear()
+            print(HEADER)
+
+        # Print all users in the chatroom in the server terminal
+        elif cmd.lower() == 'list':
+            for client in ADDRESSES:
+                try:
+                    print('Client: \'{}\' ~ {} : {}'.format(USERS[client],
+                                                            ADDRESSES[client][0], ADDRESSES[client][1]))
+                except:
+                    print(
+                        'Connection: {} : {}'.format(ADDRESSES[client][0], ADDRESSES[client][1]))
+
+        # Quits the server
+        # Close each socket to all connected users
+        elif cmd.lower() == 'quit':
+            for client in ADDRESSES:
+                try:
+                    print('[*] Closing Client: \'{}\' ~ {} : {}'.format(USERS[client],
+                                                                        ADDRESSES[client][0], ADDRESSES[client][1]))
+                except:
+                    print(
+                        '[*] Closing Connection: {} : {}'.format(ADDRESSES[client][0], ADDRESSES[client][1]))
+                client.close()
+            os._exit(1)
+
+        # Not a valid server command
+        else:
+            print(
+                'Not a valid server command\nType "help" to see the list of server commands')
+
+
+# Puts the server into listening mode
+# Starts the thread on accepting all client connections to the server.
+def Main():
+    SERVER.listen(5)
+    ACCEPT_THREAD = threading.Thread(target=establish_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
+
+
+# Clear the screen on start and display the main HEADER.
+# Then jumps to the main TCP related stuff.
+if __name__ == '__main__':
+    clear()
+    print(HEADER)
+    Main()
